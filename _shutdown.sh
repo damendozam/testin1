c28e63207d64703ab3ae39147a65bbc256ce3723
damendozam <damendozam@unal.edu.co>
2020-01-24 19:30:55 -0500

    .

--- _shutdown.sh
+++ _shutdown.sh
@@ -5,6 +5,7 @@ sudo echo "in" > /sys/class/gpio/gpio3/direction # Status pin (in or out)
 
 while true; do
 	state=$(cat /sys/class/gpio/gpio3/value)
+	echo $state
 	if [ "$state" = "0" ];then # Condition of the button state
 		shutdown 0
 	fi
